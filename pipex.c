c0f1ead85d155ce5f124cee09731275920c7d6c6
dediane <ddecourt@student.42.fr>
2021-10-05 19:36:46 +0200

    ff

--- pipex.c
+++ pipex.c
@@ -16,7 +16,6 @@
 char *find_path(char *cmd, char **path_array)
 {
 	char *path;
-	char *tmp;
 	int i;
 
 	i = -1;
@@ -24,49 +23,65 @@ char *find_path(char *cmd, char **path_array)
 	{
 		path = ft_strjoin(path_array[i], "/");
 		path = ft_strjoin(path, cmd);
-		if (access(path, F_OK) == 0);
+		if (access(path, F_OK) == 0)
 			return(path);
 	}
+	return (NULL);
 }
 
-/*int	execute_process(char *path, char *cmd, int out,  int in)
+/*int	execute_child_process(char **path_array, char **cmd, int pipe_fd, int fd)
 {
-	//execve(cmd, cmd, path, NULL);
+	char *path;
+
+	dup2(pipe_fd, STDOUT_FILENO);
+	path = find_path(cmd[0], path_array);
+	printf("%s\n", path);
+	printf("fd -> %d\n", fd);
+	return (0);
 }*/
 
 int main(int argc, char **argv, char **envp)
 {
 	int fd[2];
-	int pipe_fd[2];
-	int pid;
+	//int pipe_fd[2];
+	//int pid;
 	char **path_array;
 	char **cmd;
 
 	fd[0] = open(argv[1], O_RDONLY);
 	if (fd[0] == -1)
 		perror(argv[1]);
-	fd[1] = open(argv[argc - 1], O_WRONLY | O_CREAT);
+	fd[1] = open(argv[argc - 1], O_WRONLY | O_CREAT, 0644);
 	if (fd[1] == -1)
 		perror(argv[argc - 1]);
  	path_array = get_path(envp);
 	cmd = get_cmd_array(argv, argc);
 	if (path_array == NULL)
 		return (1);
- 	if (pipe(pipe_fd) == -1)
+	printf("%s\n", cmd[0]);
+	printf("%s\n",find_path(cmd[0], path_array));
+	execve(find_path(cmd[0], path_array), &cmd[1], envp);
+ 	/*if (pipe(pipe_fd) == -1)
  		return (1);
 	//pipe_fd[0] -> reading pipe
 	//pipe_fd[1] -> writing pipe
+	char *path;
+	path = find_path(cmd[0], path_array);
  	pid = fork();
 	//child process part 
  	if (pid == 0)
- 	{
- 		dup2(pipe_fd[1], STDOUT_FILENO);
- 	}
+	{
+		dup2(pipe_fd[1], STDOUT_FILENO);
+	}
+	//execute_child_process(path_array, cmd, pipe_fd[1], fd[0]);
 	//parent process part
 	else 
 	{
 		
 	}
- 	waitpid(pid, NULL, 0);
+	close(pipe_fd[0]);
+	close(fd[0]);
+	close(fd[1]);
+ 	waitpid(pid, NULL, 0);*/
  	return (0);
 }
