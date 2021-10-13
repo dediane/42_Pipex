506aa5372256a22656ff1cdc5705d48c55b58e08
dediane <ddecourt@student.42.fr>
2021-10-13 16:11:15 +0200

    ss

--- bonus/pipex_utils_bonus.c
+++ bonus/pipex_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "pipex_bonus.h"
+#include "../pipex.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
